a7551efbe9ad03dbc68efd21364b8e0a75dd2148
sooraj <tpsooraj7@gmail.com>
2024-11-14 11:30:17 +0530

    added base route for index

--- apk/urls.py
+++ apk/urls.py
@@ -1,7 +1,7 @@
 from .import views
 from django.urls import path
 urlpatterns=[
-    
+    path('',views.index,name='index'),
     path('index/',views.index,name='index'),
     path('register/',views.register,name='register'),
     path('login/',views.login,name='login'),
